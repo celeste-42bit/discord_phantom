26713eee4ce7e864df59c57c858687339591def9
ᑢᗩᘿᒪᘿSᖶᘿ <studi.jasha.gruener@gmail.com>
2023-07-18 22:55:22 +0200

    Started anew!

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-from phantom import *
+import discord
+from discord.ext import commands
 
-class App(Phantom):
-    def __init__(self) -> None:
-        pass
-    
-    Phantom
+
+intents = discord.Intents.default()
+intents.typing = False
+intents.presences = False
+intents.message_content = True
+intents.reactions = False
+        
+description = "The Phantom by 'The Phantasm Bot Projects'"
+        
+bot = commands.Bot(intents = intents, description = description, command_prefix='/', debug_guild=1090933390007615551)
+        
+with open('.token', 'r') as tkn:
+    token = str(tkn.read())
+bot.run(token)
+
+@bot.event
+async def on_ready():
+    await bot.change_presence(activity=discord.CustomActivity(name="Type /help"))
+    print(f'I logged in as {bot.user} (ID: {bot.user.id})')
--- interfaces/reactionInterface.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-class reactionInterface(ABC):
-    @abstractmethod
-    def addReaction(self):
-        pass
-    
-    @abstractmethod
-    def removeReaction(self):
-        pass
