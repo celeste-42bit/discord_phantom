9a65614eb2c92ce2c574abb144dc4f07097a257c
ᑢᗩᘿᒪᘿSᖶᘿ <studi.jasha.gruener@gmail.com>
2023-07-22 10:51:11 +0200

    Next commit...

--- app.py
+++ app.py
@@ -14,24 +14,29 @@ record = False  # Should the bot record the chat?
 if record:
     follow = True
         
-description = 'The Phantom by "The Phantasm Bot Projects"'                                  # The bots description (No idea, what it actually does)
+description = 'The Phantom by "The Phantasm Bot Projects"'                                  # The bots description (This appears in the default help page)
 status = discord.Status.online                                                              # The bots status (The dot on the profile picture)
 activity = discord.Activity(type=discord.ActivityType.watching, name="over the Phantasm")   # The bots activity (The text under its name)
+shard_id = 0                                                                                # Shard ID
+shard_count = 2                                                                             # Total number of shards
         
 client = commands.Bot(
     command_prefix='/',
     intents = intents,
     description = description,
+    help_command=commands.DefaultHelpCommand(),
     status=status,
     activity=activity,
-    case_insensitive=True
+    case_insensitive=True,
+    shard_id = shard_id,
+    shard_count = shard_count
     )
 
 @client.event
 async def on_ready():
     print(f'I logged in as {client.user} (ID: {client.user.id}). At your service, mistress!')
 
-'''Issue: This function blocks all commands from executing
+'''on_message function - This function is commented out, since it is blocking command execution. Refer to GitHub issue #1 https://github.com/celeste-42bit/discord_phantom/issues/1 
 @client.event()
 async def on_message(message):
     if message.author == client.user:
@@ -47,22 +52,45 @@ async def on_message(message):
 
 @client.command(name='roll', brief='Roll the dice!', help='Usage: /roll <dice ammount> <hunger>')
 async def roll(ctx, num_dice, hunger):
+    tf = discord.utils.get(ctx.guild.emojis, name='total_failure')
+    f = discord.utils.get(ctx.guild.emojis, name='failure')
+    s = discord.utils.get(ctx.guild.emojis, name='success')
+    cs = discord.utils.get(ctx.guild.emojis, name='critical_success')
+    htf = discord.utils.get(ctx.guild.emojis, name='hunger_total_failure')
+    hf = discord.utils.get(ctx.guild.emojis, name='hunger_failure')
+    hs = discord.utils.get(ctx.guild.emojis, name='hunger_success')
+    hcs = discord.utils.get(ctx.guild.emojis, name='hunger_critical_success')
     dice = []
+    concat = []
     for i in range(int(num_dice)):
         dice.append(roll_the_dice())
-    # print(dice)
-    await ctx.send(str(dice))
+    for i in range(int(num_dice)):
+        match dice[i]:
+            case 0:
+                concat.append(tf)
+            case 1:
+                concat.append(f)
+            case 2:
+                concat.append(s)
+            case 3: concat.append(cs)
+
+    await ctx.send(str(concat))
 
 def roll_the_dice():
     roll = random.randint(1, 10)  # roll a d10
-    if roll == 1:
-        return 0  # Return total failure
-    elif roll == 10:
-        return 3  # Return critical success
-    elif roll > 5:
-        return 2  # Return success
-    else:
-        return 1  # Return failure
+    match roll:  # match to all possible dice states
+        case 1:
+            return 0  # total failure
+        case 10:
+            return 3  # critical success
+        case x if x > 5:
+            return 2  # success
+        case x if x < 5:
+            return 1  # failure
+
+@client.command(name='hi', brief='Check whether the bot is available', help='Usage: /hi')
+async def hi(ctx):
+    await ctx.send(f'Heyo, {ctx.author.name}!')
 
 with open('.token', 'r') as tkn:
     token = str(tkn.read())
---
+++ logger.py
@@ -0,0 +1,22 @@
+import os, time
+
+def log(message):
+    # Get the size of the log file in bytes
+    size = os.path.getsize('./logs/message.log')
+
+    # If the log file exceeds 10MB, delete it and create a new one
+    if size >= 10000000:  # 10MB in bytes
+        print('The log file was bigger then 10MB and has been deleted')
+        os.remove('./logs/message.log')
+        create()
+    # Write the message to the log file
+    with open('./logs/message.log', '+a', encoding='utf-8') as log:
+        log.write(f'{message}\n')
+
+def create():
+    print('A new log file has been created')
+    with open('./logs/message.log', '+w', encoding='utf-8') as log:  # If writable file doesn't exist, create first, then write...
+        log.write(f'This log was created at {time.gmtime()}\n')
+
+if not os.path.exists('./logs/message.log'):
+    create()
